126b051d6fd66d1b9a8be7dcfc8deece55eefe29
Caroline Artz <ceartz@gmail.com>
2019-12-30 17:57:47 -0600

    first pass animations.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,10 +4,6 @@ import Helmet from 'react-helmet';
 import { Intro } from 'components/intro/Intro';
 import { Container } from 'components/container/Container';
 import BongoCat from 'assets/svg/bongo-cat.svg';
-// import { BlockText } from 'components/block-text/BlockText';
-
-// import { Logo } from 'components/logo/Logo';
-// import { Row } from 'components/row/Row';
 
 export default () => {
   return (
@@ -18,29 +14,6 @@ export default () => {
       <Container>
         <BongoCat />
       </Container>
-      {/* <Row>
-          <WaveHello />
-        </Row>
-        <Row>
-          <Logo />
-        </Row>
-        <Row>
-        </Row>
-      </Intro> */}
-
-      {/* <BlockText
-        heading="Who we are"
-        description={
-          <>
-            Ueno is a full-service agency, busy designing and building beautiful digital products,
-            brands, and experiences. For more informations go to{' '}
-            <a href="https://ueno.co" target="_blank" rel="noopener noreferrer">
-              ueno.co
-            </a>
-            .
-          </>
-        }
-      /> */}
     </>
   );
 };

6ae160d788aa20dabfd7cdf3a971120b420c0641
Caroline Artz <ceartz@gmail.com>
2019-12-30 14:03:46 -0600

    add caroline animation.

--- src/components/logo/Logo.tsx
+++ src/components/logo/Logo.tsx
@@ -1,240 +1,106 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 /* eslint-disable @typescript-eslint/no-unused-vars */
-
-// import React, { PureComponent } from 'react';
-// import PropTypes from 'prop-types';
-
-// import { TimelineLite } from 'gsap';
-// import { add } from 'gsap-tools';
-
-// export default class Hero extends PureComponent {
-
-//   static propTypes = {
-//     children: PropTypes.node,
-//   }
-
-//   componentDidMount() {
-//     setTimeout(this.animate, 1000);
-//   }
-
-//   componentWillUnmount() {
-//     this.disposer1();
-//     // this.disposer2();
-//     // this.disposer3();
-//     // this.disposer4();
-//   }
-
-//   animate = () => {
-//     const mainTimeline = new TimelineLite({ id: 'Everything' });
-//     const circlesTimeline = new TimelineLite({ id: 'Circles' });
-//     const logoTimeline = new TimelineLite({ id: 'Logo' });
-//     const appleTimeline = new TimelineLite({ id: 'Apple Guy' });
-
-//     mainTimeline.addLabel('start');
-
-//     // get external timelines
-//     circlesTimeline.add(this.circles.timelineEnter).add(this.circles.timelineLeave);
-//     logoTimeline.add(this.logo.timelineEnter).add(this.logo.timelineLeave, '+=0.75');
-//     appleTimeline.add(this.apple.timelineEnter);
-
-//     mainTimeline
-//       .add(circlesTimeline, 'start')
-//       .add(logoTimeline, 'start')
-//       .add(appleTimeline, logoTimeline.duration());
-
-//     this.disposer1 = add(mainTimeline);
-//     // this.disposer2 = add(circlesTimeline);
-//     // this.disposer3 = add(logoTimeline);
-//     // this.disposer4 = add(appleTimeline);
-//   }
-
-//   render() {
-//     const { children } = this.props;
-
-//     return (
-//       <div className="hero">
-//         <div className="hero__inner">
-//           <Circles ref={(el) => { this.circles = el; }} />
-//           <Logo ref={(el) => { this.logo = el; }} />
-//           <AppleGuy ref={(el) => { this.apple = el; }} />
-//           {children}
-//         </div>
-//       </div>
-//     );
-//   }
-// }
-
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import React, { PureComponent } from 'react';
-// import PropTypes from 'prop-types';
+import { gsap } from 'gsap';
+
 import CarolineLogo from 'assets/svg/caroline-logo.svg';
-
-// let blah: GreenSockGlobals
-
-// import { gsap } from 'gsap';
-// import { CSSRulePlugin } from 'gsap/CSSRulePlugin';
-// import { ScrollToPlugin } from 'gsap/ScrollToPlugin';
-// import CustomEase from 'gsap/';
-// import { DrawSVGPlugin } from 'gsap/DrawSVGPlugin';
-// import { GSDevTools } from 'gsap/GSDevTools';
-// import { InertiaPlugin } from 'gsap/InertiaPlugin';
-// import { MorphSVGPlugin } from 'gsap/MorphSVGPlugin';
-// import { SplitText } from 'gsap/SplitText';
-// import { CustomBounce } from 'gsap/CustomBounce';
-
-// gsap.registerPlugin(
-//   CSSRulePlugin,
-//   ScrollToPlugin,
-//   CustomEase,
-//   DrawSVGPlugin,
-//   GSDevTools,
-//   InertiaPlugin,
-//   MorphSVGPlugin,
-//   SplitText,
-//   CustomBounce
-// );
-
-// import DrawSVG from 'gsap/DrawSVGPlugin';
-
-// import { CustomBounce } from 'gsap/EasePack';
-
-import { gsap, TweenMax, TimelineMax, Linear, Power4 } from 'gsap';
-import { add } from 'gsap-tools';
+import WaveHello from 'assets/svg/hello.svg';
 import s from './Logo.scss';
 
+// TODO: if env === dev
 (window as any).gsap = gsap;
 
-// import 'gsap/CustomEase';
-// debugger;
+require('gsap/DrawSVGPlugin');
+require('gsap/CustomEase').CustomEease;
 
-// declare let CustomBounce: any;
-// declare let DrawSVGPlugin: any;
-
-// import 'gsap/CustomBounce';
-
-const CustomEase = require('gsap/CustomEase').CustomEease;
-const CustomBounce = require('gsap/CustomBounce');
-const DrawSVG = require('gsap/DrawSVGPlugin');
-
-// import 'gsap/CustomBounce';
-
-// debugger;
-// interface ILogoProps {
-//   children: any;
-// }
-
-// export const Logo = ({children}: ILogoProps) => {
-//   const renderLogo  = () => {
-
-//   }s
-// }
-
-// declare let CustomBounce: any;
-// declare const DrawSVGPlugin: any;
-
-// const plugins = [PixiPlugin];
-// PixiPlugin.registerPIXI(PIXI);
+const CustomBounce = require('gsap/CustomBounce').CustomBounce;
 
 export class Logo extends PureComponent {
-  disposer1: any;
+  logoTimeline: GSAPStatic.Timeline;
+  waveTimeline: GSAPStatic.Timeline;
 
-  // static propTypes: ILogoProps = {
-  //   children: PropTypes.node,
-  // };
+  constructor(props: any) {
+    super(props);
+
+    this.logoTimeline = gsap.timeline({ delay: 1, defaults: { ease: 'power4.out' } });
+    this.waveTimeline = gsap.timeline({
+      repeat: 3,
+      defaults: { ease: 'power2.inOut', duration: 0.1 },
+    });
+  }
 
   componentDidMount() {
     this.animate();
-    // setTimeout(this.animate, 2000);
   }
 
-  componentWillUnmount() {
-    this.disposer1();
-    // this.disposer2();
-    // this.disposer3();
-    // this.disposer4();
-  }
+  wave = () => {
+    // scaling
+    gsap.set('#hello', {
+      scale: 0.5,
+      transformOrigin: '50% 50%',
+    });
+
+    // waving
+    gsap.set('#hand', {
+      transformOrigin: '90% 90%',
+      rotation: 20,
+    });
+    return this.waveTimeline.to('#hand', { rotation: -2 }).to('#hand', { rotation: 15 });
+  };
+
+  logo = () => {
+    const drawSVGLinear = { drawSVG: '100%', ease: 'none' };
+
+    gsap.set('#caroline-logo .path', { drawSVG: '0% 0%' });
+    gsap.set('.dot-group', { yPercent: 100 });
+    CustomBounce.create('myBounce', { strength: 0.6, squash: 2 });
+
+    return this.logoTimeline
+      .set('.logo', { autoAlpha: 1 })
+      .to('.cPath', { duration: 0.5, stagger: 0.2, ...drawSVGLinear })
+      .to('.a1Path', { duration: 0.25, stagger: 0.2, ...drawSVGLinear }, '-=0.35')
+      .to('.a2Path', { duration: 0.25, stagger: 0.1, ...drawSVGLinear }, '-=0.04')
+      .to('.r1Path', { duration: 0.4, stagger: 0.1, ...drawSVGLinear }, '-=0.02')
+      .to('.r2Path', { duration: 0.3, stagger: 0.07, ...drawSVGLinear }, '-=0.10')
+      .to('.o1Path', { duration: 0.5, stagger: 0.15, ...drawSVGLinear }, '-=0.20')
+      .to('.o2Path', { duration: 0.5, stagger: 0.15, ...drawSVGLinear }, '-=0.20')
+      .to('.l1Path', { duration: 0.5, stagger: 0.15, ...drawSVGLinear }, '-=0.20')
+      .to('.l2Path', { duration: 0.35, stagger: 0.2, ...drawSVGLinear }, '-=0.20')
+      .to('.i1Path', { duration: 0.2, stagger: 0.07, ...drawSVGLinear }, '-=0.10')
+      .to('.i2Path', { duration: 0.2, stagger: 0.07, ...drawSVGLinear }, '-=0.10')
+      .to('.n1Path', { duration: 0.2, stagger: 0.07, ...drawSVGLinear }, '-=0.10')
+      .to('.n2Path', { duration: 0.2, stagger: 0.07, ...drawSVGLinear }, '-=0.10')
+      .to('.e1Path', { duration: 0.2, stagger: 0.07, ...drawSVGLinear }, '-=0.10')
+      .to('.e2Path', { duration: 0.2, stagger: 0.07, ...drawSVGLinear }, '-=0.10')
+      .from('#dot', 0.01, { autoAlpha: 0 }, '-=0.03')
+      .to('#dot', 0.4, { yPercent: -500, ease: 'power4.out' }, '-=0.01')
+      .to('.dot-group', 0.4, { scale: 1.1, ease: 'power4.out' }, '-=0.4')
+      .to('#dot', 0.9, { yPercent: -150, ease: 'myBounce' })
+      .to('#dot', 0.9, {
+        scaleY: 0.6,
+        scaleX: 1.2,
+        ease: 'myBounce-squash',
+        // transformOrigin: 'bottom',
+        delay: -0.9,
+      });
+  };
 
   animate = () => {
-    TweenMax.set('#caroline-logo .path', { drawSVG: '0 0' });
-    TweenMax.set('.dot-group', { yPercent: 100 });
-
-    CustomBounce.CustomBounce.create('bounceSquash', {
-      strength: 0.6,
-      squash: 2,
-    });
-
-    CustomBounce.CustomBounce.create('bounceSquash2', {
-      strength: 0.6,
-      squash: 2,
-    });
-    const drawSVGLinear = { drawSVG: '100%', ease: Linear.easeNone };
-
-    const tl = new TimelineMax();
-    this.disposer1 = add(tl);
-
-    tl.addLabel('i', 2.85)
-      .set('#clips', { autoAlpha: 1 })
-      .staggerTo('.cPath', 0.5, drawSVGLinear, 0.2)
-      .staggerTo('.a1Path', 0.25, drawSVGLinear, 0.2, '-=0.35')
-      .staggerTo('.a2Path', 0.25, drawSVGLinear, 0.1, '-=0.04')
-      .staggerTo('.r1Path', 0.4, drawSVGLinear, 0.1, '-=0.02')
-      .staggerTo('.r2Path', 0.3, drawSVGLinear, 0.07, '-=0.10')
-      .staggerTo('.o1Path', 0.5, drawSVGLinear, 0.15, '-=0.20')
-      .staggerTo('.o2Path', 0.5, drawSVGLinear, 0.15, '-=0.20')
-      .staggerTo('.l1Path', 0.5, drawSVGLinear, 0.15, '-=0.20')
-      .staggerTo('.l2Path', 0.35, drawSVGLinear, 0.2, '-=0.20')
-      .staggerTo('.i1Path', 0.2, drawSVGLinear, 0.07, '-=0.10')
-      .staggerTo('.i2Path', 0.2, drawSVGLinear, 0.07, '-=0.10')
-      .staggerTo('.n1Path', 0.2, drawSVGLinear, 0.07, '-=0.10')
-      .staggerTo('.n2Path', 0.2, drawSVGLinear, 0.07, '-=0.10')
-      .staggerTo('.e1Path', 0.2, drawSVGLinear, 0.07, '-=0.10')
-      .staggerTo('.e2Path', 0.2, drawSVGLinear, 0.07, '-=0.10')
-      .from('#dot', 0.01, { autoAlpha: 0 }, '-=0.03')
-      .to('#dot', 0.4, { yPercent: -500, ease: Power4.easeOut }, '-=0.01')
-      .to('.dot-group', 0.4, { scale: 1.4, ease: Power4.easeOut }, '-=0.4')
-      .to('#dot', 0.9, { yPercent: -150, ease: 'bounceSquash' })
-      .to(
-        '#dot',
-        0.9,
-        {
-          scaleY: 0.6,
-          scaleX: 1.2,
-          ease: 'bounceSquash2-squash',
-          transformOrigin: '50% bottom',
-          delay: -0.9,
-        },
-        0
-      )
-      .staggerTo('.iPath', 0.5, drawSVGLinear, 0.05, 'i')
-      .staggerTo('.ePath', 0.5, drawSVGLinear, 0.05, '-=0.3');
-
-    // const mainTimeline = new TimelineLite({ id: 'Everything' });
-    // const circlesTimeline = new TimelineLite({ id: 'Circles' });
-    // const logoTimeline = new TimelineLite({ id: 'Logo' });
-    // const appleTimeline = new TimelineLite({ id: 'Apple Guy' });
-
-    // mainTimeline.addLabel('start');
-
-    // // get external timelines
-    // circlesTimeline.add(this.circles.timelineEnter).add(this.circles.timelineLeave);
-    // logoTimeline.add(this.logo.timelineEnter).add(this.logo.timelineLeave, '+=0.75');
-    // appleTimeline.add(this.apple.timelineEnter);
-
-    // mainTimeline
-    //   .add(circlesTimeline, 'start')
-    //   .add(logoTimeline, 'start')
-    //   .add(appleTimeline, logoTimeline.duration());
-
-    // this.disposer1 = add(tl);
-    // this.disposer2 = add(circlesTimeline);
-    // this.disposer3 = add(logoTimeline);
-    // this.disposer4 = add(appleTimeline);
+    gsap
+      .timeline()
+      .add(this.wave())
+      .add(this.logo());
   };
 
   render() {
     return (
-      <div className={s.logo__container}>
-        <CarolineLogo className={s.logo__graphic} />
+      <div>
+        <div className={s.hello__container}>
+          <WaveHello className={s.wave__graphic} />
+        </div>
+        <div className={s.logo__container}>
+          <CarolineLogo className={s.logo__graphic} />
+        </div>
       </div>
     );
   }

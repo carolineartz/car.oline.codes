4d30a370a6e538719e1cc7fe6db933f9b4679a1e
Caroline Artz <ceartz@gmail.com>
2020-01-10 18:00:34 -0600

    fix temp content.

--- src/pages/page-2.tsx
+++ src/pages/page-2.tsx
@@ -2,13 +2,13 @@ import React from "react"
 import Helmet from "react-helmet"
 
 import { Link } from "gatsby"
-import { Paragraph, Heading } from "grommet"
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { Heading } from "grommet"
 
 export default () => (
   <>
     <Helmet title="Page 2" />
-    <Heading margin="none">Whoops...!</Heading>
-    <Paragraph margin="none">Welcome to page 2</Paragraph>
+    <Heading margin="none">Welcome to page 2</Heading>
     <Link to="/">Go back to the homepage</Link>
   </>
 )

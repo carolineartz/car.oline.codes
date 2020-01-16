8746b2666809b2976e5b3115676aa09b36d2f9a5
Caroline Artz <ceartz@gmail.com>
2020-01-16 17:43:00 -0600

    Maybe ok...

--- src/animation/LanguageStatusAnimation.ts
+++ src/animation/LanguageStatusAnimation.ts
@@ -1,11 +1,16 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { gsap, Elastic } from "gsap"
-import { sampleSize, random, round } from "lodash"
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { sampleSize, random, round, without } from "lodash"
 import { PointPosition } from "../hooks/use-point-position"
 
 import SplitText from "gsap/SplitText"
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import GSDevTools from "gsap/GSDevTools"
 
 require("animation/bezier-easing.min.js")
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 class Picker<T> {
   private readonly pickCount: () => number
   readonly items: T[] // can be private but public now for logging
@@ -38,14 +43,14 @@ type Rotations = {
 class LetterTransformations {
   static rotations(letterCount: number): Rotations[] {
     return Array.from({ length: letterCount }, () => {
-      const tr: number = round(random(0, 6, true), 2)
-      return { pos: tr, neg: -tr }
+      const rr: number = round(random(0, 6, true), 2)
+      return { pos: rr, neg: -rr }
     })
   }
 
   static translations(letterCount: number): Translations[] {
     return Array.from({ length: letterCount }, () => {
-      const tr: number = round(random(10, 50, true), 2)
+      const tr: number = round(random(5, 10, true), 2)
       return { pos: tr, neg: -tr }
     })
   }
@@ -63,19 +68,19 @@ export class LanguageStatusAnimation {
 
   private element: HTMLElement
   private _letters: HTMLElement[] // the randomly picked ones; don't
-  private prevLetters: HTMLElement[]
-  private picker: Picker<HTMLElement>
+  private numChars: number
+  private timeline: GSAPTimeline
 
   constructor(element: HTMLElement) {
     this.element = element
     const splitText = new SplitText(element)
 
     this.characters = splitText.chars
-    this.picker = new Picker(this.characters)
-
-    this.prevLetters = []
-    this._letters = this.picker.pick()
+    this.numChars = random(3, 5)
+    this._letters = sampleSize(this.characters, 3)
     this.init()
+    this.timeline = gsap.timeline({ autoRemoveChildren: true })
+    // GSDevTools.create({ animation: this.timeline })
   }
 
   get letters() {
@@ -87,20 +92,12 @@ export class LanguageStatusAnimation {
   }
 
   init() {
-    this.element.addEventListener(
-      "mouseenter",
-      function(this: LanguageStatusAnimation) {
-        this.updateLetters().then(
-          function(this: LanguageStatusAnimation) {
-            if (this.prevLetters) {
-              this.resetTranslations(this.prevLetters)
-            }
-          }.bind(this)
-        )
-      }.bind(this)
-    )
+    if (this.element.firstElementChild) {
+      ;(this.element.firstElementChild as HTMLElement).style.display = "inline-block"
+    }
+    this.element.addEventListener("mouseenter", this.updateLetters.bind(this))
     this.element.addEventListener("mousemove", this.translateLetters.bind(this))
-    this.element.addEventListener("mouseleave", this.resetTranslations.bind(this, this.letters))
+    this.element.addEventListener("mouseleave", this.resetTranslations.bind(this))
   }
 
   animate(pointPosition: PointPosition) {
@@ -108,41 +105,52 @@ export class LanguageStatusAnimation {
   }
 
   // mouseenter
-  private updateLetters(
-    this: LanguageStatusAnimation
-  ): Promise<{ prevLetters: HTMLElement[]; letters: HTMLElement[] }> {
-    console.log(`%c Enter`, "color: #7cbd64")
-    this.prevLetters = this._letters
-    this._letters = this.picker.pick()
+  private updateLetters(this: LanguageStatusAnimation): void {
+    // this.timeline.tweenTo("foo")
+    // this.timeline.removeLabel("foo")
 
-    return Promise.resolve({
-      prevLetters: this.prevLetters,
-      letters: this._letters,
-    })
+    console.log(`%c Enter`, "color: #5FC462")
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    // const prevLetters: HTMLElement[] = this.letters
+    const shuffled = this.characters.sort(() => 0.5 - Math.random())
+    this._letters = shuffled.slice(0, this.numChars)
+    // this._letters = without(shuffled.slice(0, this.numChars), ...prevLetters)
+    console.log(this.element)
   }
 
   private translateLetters(this: LanguageStatusAnimation): void {
     console.log(`%c Over`, "color: #FF7349")
-    if (!this.pointPosition) return
+
+    const bounds = this.element.getBoundingClientRect()
+
+    const cursorPosition: { x: number; y: number } = this.pointPosition || {
+      x: bounds.x + bounds.width / 2,
+      y: bounds.y + bounds.height / 2,
+    }
+
     // Document scrolls
     const docScrolls = {
       left: document.body.scrollLeft + document.documentElement.scrollLeft,
       top: document.body.scrollTop + document.documentElement.scrollTop,
     }
-    const bounds = this.element.getBoundingClientRect()
     // Mouse position relative to this.element
     const relmousepos = {
-      x: this.pointPosition.x - bounds.left - docScrolls.left,
-      y: this.pointPosition.y - bounds.top - docScrolls.top,
+      x: cursorPosition.x - bounds.left - docScrolls.left,
+      y: cursorPosition.y - bounds.top - docScrolls.top,
     }
 
     const lettersTranslations = LetterTransformations.translations(this.letterCount)
     const lettersRotations = LetterTransformations.rotations(this.letterCount)
 
     for (const [index, letter] of this.letters.entries()) {
-      gsap.to(letter, {
-        duration: 3,
-        ease: "expo.out",
+      this.timeline.to(letter, {
+        duration: 1,
+        // ease: "expo.out",
+        ease: "power3.out",
+        // stateAt: {
+        //   x: bounds.x,
+        //   y: bounds.y,
+        // },
         y: lineEq(
           lettersTranslations[index].pos,
           lettersTranslations[index].neg,
@@ -161,46 +169,121 @@ export class LanguageStatusAnimation {
     }
   }
 
-  private resetTranslations(
-    this: LanguageStatusAnimation,
-    letters: HTMLElement[]
-  ): GSAPTimeline | undefined {
-    console.log(`%c Exit`, "color: #B13254")
-    // return () => {
-    //   return requestAnimationFrame(() => {
-    // if we're on a touch device or something
-    if (!this.pointPosition || !this.pointPosition.directionX || !this.pointPosition.directionY) {
-      return
-    }
+  private reset() {
+    const time = this.timeline.progress()
+    this.timeline.to(
+      this.characters,
+      {
+        duration: 1,
+        rotation: 0,
+        ease: "power4.out",
+        y: 0,
+        onComplete: this.timeline.clear,
+      },
+      time
+    )
+  }
 
-    return gsap
-      .timeline()
-      .to(
-        letters,
-        {
-          duration: 0.2,
-          ease: "quad.out",
-          y: this.pointPosition.directionY === "up" ? "-=80%" : "+=80%",
-          rotation: this.pointPosition.directionY === "up" ? "-=10" : "+=10",
-          opacity: 0,
-        },
-        0
-      )
-      .to(
-        letters,
-        {
-          duration: random(0.5, 0.7, true),
-          ease: Elastic.easeOut.config(1, 0.4),
-          startAt: {
-            y: this.pointPosition.directionY === "up" ? "80%" : "-80%",
+  private resetTranslations(this: LanguageStatusAnimation): void {
+    console.log(`%c Exit`, "color: #B13254")
+
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    // const children = this.timeline.getChildren()
+    this.timeline.totalProgress(1)
+    this.timeline.clear()
+    this.timeline.kill()
+
+    this.timeline = gsap.timeline()
+    // eslint-disable-next-line no-debugger
+    // debugger
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const rand = random(0.5, 0.7, true)
+    const blah = () =>
+      gsap
+        .timeline()
+        .to(
+          this.letters,
+          {
+            duration: 0.2,
+            ease: "quad.out",
+            // eslint-disable-next-line @typescript-eslint/no-use-before-define
+            y: pointPositionConditionalWithDefault("up", this.pointPosition, {
+              ifTrue: "-=80%",
+              ifFalse: "+=80%",
+            }),
+            // eslint-disable-next-line @typescript-eslint/no-use-before-define
+            rotation: pointPositionConditionalWithDefault("up", this.pointPosition, {
+              ifTrue: "-=10",
+              ifFalse: "+=10",
+            }),
             opacity: 0,
           },
-          stagger: 0.02,
-          y: "0%",
-          rotation: 0,
-          opacity: 1,
-        },
-        0.2
-      )
+          0
+        )
+        .to(
+          this.letters,
+          {
+            duration: rand,
+            ease: "elastic.out",
+            startAt: {
+              // eslint-disable-next-line @typescript-eslint/no-use-before-define
+              y: pointPositionConditionalWithDefault("up", this.pointPosition, {
+                ifTrue: "-80%",
+                ifFalse: "80%",
+              }),
+              opacity: 0,
+            },
+            stagger: 0.02,
+            y: "0%",
+            opacity: 1,
+          },
+          "0.2"
+        )
+        .to(
+          this.letters,
+          {
+            // y: "0",
+            duration: rand,
+            stagger: 0.02,
+            rotation: "0%",
+            ease: "power1.out",
+          },
+          "+=0.19"
+        )
+        .call(this.reset.bind(this))
+    // GSDevTools.create({ animation: blah })
+    this.timeline.add(blah(), "foo")
+    console.log("end time", this.timeline.endTime())
+  }
+}
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+function pointPositionConditionalWithDefault(
+  direction: "up" | "right" | "down" | "left",
+  pointPosition?: PointPosition,
+  {
+    ifTrue = "0%",
+    ifFalse = "0%",
+    otherwise = "0%",
+  }: { ifTrue?: string | number; ifFalse?: string | number; otherwise?: string | number } = {}
+): string | number {
+  if (!pointPosition) {
+    return otherwise
+  }
+  switch (direction) {
+    case "up":
+    case "down":
+      if (!pointPosition.directionY) {
+        return otherwise
+      } else {
+        return pointPosition.directionY === direction ? ifTrue : ifFalse
+      }
+    case "right":
+    case "left":
+      if (!pointPosition.directionX) {
+        return otherwise
+      } else {
+        return pointPosition.directionX === direction ? ifTrue : ifFalse
+      }
   }
 }

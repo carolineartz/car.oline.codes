1df37cf45e590f2b83494f3b694aef96a6a71300
Caroline Artz <ceartz@gmail.com>
2021-05-10 11:38:38 -0500

    Simplify app component.

--- src/App.tsx
+++ src/App.tsx
@@ -6,25 +6,16 @@ import { theme } from "@/config/theme";
 import { Fade } from "react-awesome-reveal";
 import { NameIntro } from "@/components/NameIntro"
 
-const AppContent = () => {
-  return (
-    <>
-      <Fade triggerOnce>
-        <NameIntro />
-      </Fade>
-      <Box id="section-portfolio" pad="medium" tag="section" background="white">
-        <Projects />
-      </Box>
-    </>
-  );
-};
-
-
 export const App = () => {
   return (
     <Grommet css="min-height: 100vh" theme={theme}>
       <GlobalStyles />
-      <AppContent />
+      <Fade triggerOnce>
+        <NameIntro />
+      </Fade>
+      <Box id="section-portfolio" pad="medium" tag="section">
+        <Projects />
+      </Box>
     </Grommet>
   );
 };
